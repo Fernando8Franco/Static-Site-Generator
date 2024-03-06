1f4eeb72dc2c842abdf74b512ff5c0393b4e072a
Fernando8Franco <francoduranluisfernando@gmail.com>
2024-03-06 11:48:45 -0600

    Default values added

--- src/textnode.py
+++ src/textnode.py
@@ -1,5 +1,5 @@
 class TextNode:
-    def __init__(self, text, text_type, url=None):
+    def __init__(self, text=None, text_type=None, url=None):
         self.text = text
         self.text_type = text_type
         self.url = url
