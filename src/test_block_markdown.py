abf81cb711f2851819acd0f81893a570a45fd3da
Fernando8Franco <francoduranluisfernando@gmail.com>
2024-03-12 10:21:22 -0600

    unit test added

--- src/test_block_markdown.py
+++ src/test_block_markdown.py
@@ -27,5 +27,28 @@ This is the same paragraph on a new line
             ]
         )
 
+    def test_markdown_to_blocks(self):
+        text = """
+This is **bolded** paragraph
+
+
+
+This is another paragraph with *italic* text and `code` here
+This is the same paragraph on a new line
+
+* This is a list
+* with items
+"""
+
+        blocks = markdown_to_blocks(text)
+        self.assertListEqual(
+            blocks,
+            [
+            'This is **bolded** paragraph', 
+            'This is another paragraph with *italic* text and `code` here\nThis is the same paragraph on a new line', 
+            '* This is a list\n* with items'
+            ]
+        )
+
 if __name__ == "__main__":
     unittest.main()
