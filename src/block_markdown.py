90c8c2179e8314b49def2b71d3751d9ac603b5a4
Fernando8Franco <francoduranluisfernando@gmail.com>
2024-03-12 11:44:19 -0600

    block_to_block_type function & unit tests added

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,5 +1,13 @@
-def markdown_to_blocks(markdown):
-    blocks = markdown.split("\n\n")
+import re
+from textnode import (block_type_paragraph,
+                      block_type_heading,
+                      block_type_code,
+                      block_type_quote,
+                      block_type_unordered_list,
+                      block_type_ordered_list)
+
+def markdown_to_blocks(text):
+    blocks = text.split("\n\n")
     filtered_blocks = []
 
     for block in blocks:
@@ -8,3 +16,46 @@ def markdown_to_blocks(markdown):
         filtered_blocks.append(block.strip())
 
     return filtered_blocks
+
+def block_to_block_type(block_text):
+    pattern_heading = r'^#{1,6}\s.+$'
+    if re.match(pattern_heading, block_text):
+        return block_type_heading
+    
+    if block_text[:3] == '```' and block_text[-3:] == '```':
+        return block_type_code
+    
+    lines = block_text.splitlines()
+    
+    if block_text.startswith('>'):
+        for line in lines:
+            if not line.startswith('>'):
+                return block_type_paragraph
+        
+        return block_type_quote
+    
+    if block_text.startswith('* '):
+        for line in lines:
+            if not line.startswith('* '):
+                return block_type_paragraph
+        
+        return block_type_unordered_list
+    
+    if block_text.startswith('- '):
+        for line in lines:
+            if not line.startswith('- '):
+                return block_type_paragraph
+        
+        return block_type_unordered_list
+    
+    if block_text.startswith('1. '):
+        i = 1
+        for line in lines:
+            if not line.startswith(f"{i}. "):
+                return block_type_paragraph
+            i += 1
+
+        return block_type_ordered_list
+
+
+    return block_type_paragraph
--- src/test_block_markdown.py
+++ src/test_block_markdown.py
@@ -1,6 +1,13 @@
 import unittest
 
-from block_markdown import (markdown_to_blocks)
+from block_markdown import (markdown_to_blocks,
+                            block_to_block_type)
+from textnode import (block_type_paragraph,
+                      block_type_heading,
+                      block_type_code,
+                      block_type_quote,
+                      block_type_unordered_list,
+                      block_type_ordered_list)
 
 class TestBlockMarkdown(unittest.TestCase):
     #####
@@ -49,6 +56,71 @@ This is the same paragraph on a new line
             '* This is a list\n* with items'
             ]
         )
+
+    #####
+    # TEST BLOCK TO BLOCK TYPE FUNCTION
+    #####
+    def test_block_to_block_type_paragraph(self):
+        type = block_to_block_type("This is a paragraph")
+        self.assertEqual(type, block_type_paragraph)
+
+    def test_block_to_block_type_heading(self):
+        test_cases = [
+            ("# This is a heading", block_type_heading),
+            ("## This is a heading", block_type_heading),
+            ("### This is a heading", block_type_heading),
+            ("#### This is a heading", block_type_heading),
+            ("##### This is a heading", block_type_heading),
+            ("###### This is a heading", block_type_heading)
+        ]
+        for input_text, expected_type in test_cases:
+            with self.subTest(input_text=input_text, expected_type=expected_type):
+                type = block_to_block_type(input_text)
+                self.assertEqual(type, expected_type)
+
+    def test_block_to_block_type_code(self):
+        test_cases = [
+            ("```This is a code```", block_type_code),
+            ("```This is a code\nMore code```", block_type_code),
+            ("``This is a heading``", block_type_paragraph)
+        ]
+        for input_text, expected_type in test_cases:
+            with self.subTest(input_text=input_text, expected_type=expected_type):
+                type = block_to_block_type(input_text)
+                self.assertEqual(type, expected_type)
+
+    def test_block_to_block_type_quote(self):
+        test_cases = [
+            (">quote\n>quote\n>quote\n>quote", block_type_quote),
+            (">quote", block_type_quote),
+            (">quote\nNot a quote", block_type_paragraph),
+        ]
+        for input_text, expected_type in test_cases:
+            with self.subTest(input_text=input_text, expected_type=expected_type):
+                type = block_to_block_type(input_text)
+                self.assertEqual(type, expected_type)
+
+    def test_block_to_block_type_unordered_list(self):
+        test_cases = [
+            ("* unlist\n* unlist\n* unlist", block_type_unordered_list),
+            ("- unlist", block_type_unordered_list),
+            ("- quote\nNot a list", block_type_paragraph),
+        ]
+        for input_text, expected_type in test_cases:
+            with self.subTest(input_text=input_text, expected_type=expected_type):
+                type = block_to_block_type(input_text)
+                self.assertEqual(type, expected_type)
+
+    def test_block_to_block_type_ordered_list(self):
+        test_cases = [
+            ("1. orlist\n2. orlist\n3. orlist", block_type_ordered_list),
+            ("1. orlist", block_type_ordered_list),
+            ("1. orlist\n3. orlist", block_type_paragraph),
+        ]
+        for input_text, expected_type in test_cases:
+            with self.subTest(input_text=input_text, expected_type=expected_type):
+                type = block_to_block_type(input_text)
+                self.assertEqual(type, expected_type)
 
 if __name__ == "__main__":
     unittest.main()
