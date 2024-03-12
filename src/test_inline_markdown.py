9e422d1a6bf663576e99eadfc3ee74a529700c6c
Fernando8Franco <francoduranluisfernando@gmail.com>
2024-03-11 19:46:11 -0600

    extract markdown images & extract markdown links functions added

--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -1,9 +1,12 @@
 import unittest
 
-from inline_markdown import split_nodes_delimiter
+from inline_markdown import split_nodes_delimiter, extract_markdown_images, extract_markdown_links
 from textnode import TextNode, text_type_text, text_type_bold, text_type_italic, text_type_code, text_type_link, text_type_image
 
 class TestInlineMarkdown(unittest.TestCase):
+    #####
+    ## TEST FUNCTION SPLIT NODE DELIMITER
+    #####
     def test_split_nodes_delimiter(self):
         node = TextNode("This is text with a `code block` word", text_type_text)
         new_nodes = split_nodes_delimiter([node], "`", text_type_code)
@@ -48,6 +51,33 @@ class TestInlineMarkdown(unittest.TestCase):
             ]
         )
 
+    #####
+    ## TEST FUNCTION EXTRACT MARKDOWN IMAGES
+    #####
+    def test_extract_markdown_images(self):
+        text = "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and ![another](https://i.imgur.com/dfsdkjfd.png)"
+        images = extract_markdown_images(text)
+        self.assertListEqual(
+            images,
+            [
+                ("image", "https://i.imgur.com/zjjcJKZ.png"),
+                ("another", "https://i.imgur.com/dfsdkjfd.png")
+            ]
+        )
+        
+    #####
+    ## TEST FUNCTION EXTRACT MARKDOWN LINKS
+    #####
+    def test_extract_markdown_links(self):
+        text = "This is text with a [link](https://www.example.com) and [another](https://www.example.com/another)"
+        images = extract_markdown_links(text)
+        self.assertListEqual(
+            images,
+            [
+                ("link", "https://www.example.com"),
+                ("another", "https://www.example.com/another")
+            ]
+        )
 
 if __name__ == "__main__":
     unittest.main()
